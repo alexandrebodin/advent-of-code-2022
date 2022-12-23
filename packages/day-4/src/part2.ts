bbe1c6a0bd5b42846f88e5412b1cae4138ff02e7
Alexandre Bodin <bodin.alex@gmail.com>
2022-12-23 19:17:11 +0100

    day-4 part-2

--- packages/day-4/src/part2.ts
+++ packages/day-4/src/part2.ts
@@ -1,1 +1,26 @@
-export default function part2(input: string) {}
+import { isPartialOverlap } from './utils';
+import { Range } from './types';
+
+const toRange = (str: string): Range => {
+  const [a, b] = str.split('-');
+  return [parseInt(a, 10), parseInt(b, 10)];
+};
+
+let overlaps = 0;
+
+export default function part1(input: string) {
+  const assignments = input.split('\n');
+
+  for (const assignment of assignments) {
+    const [leftAssignement, rightAssignment] = assignment.split(',');
+
+    const leftRange = toRange(leftAssignement);
+    const rightRange = toRange(rightAssignment);
+
+    if (isPartialOverlap(leftRange, rightRange)) {
+      overlaps += 1;
+    }
+  }
+
+  console.log('overlaps', overlaps);
+}
--- packages/day-4/src/utils.ts
+++ packages/day-4/src/utils.ts
@@ -6,3 +6,12 @@ export const isRangeOverlap = (rangeA: Range, rangeB: Range) => {
     (rangeB[0] >= rangeA[0] && rangeB[1] <= rangeA[1])
   );
 };
+
+export const isPartialOverlap = (rangeA: Range, rangeB: Range) => {
+  return (
+    (rangeA[0] >= rangeB[0] && rangeA[0] <= rangeB[1]) ||
+    (rangeA[1] >= rangeB[0] && rangeA[1] <= rangeB[1]) ||
+    (rangeB[1] >= rangeA[0] && rangeB[1] <= rangeA[1]) ||
+    (rangeB[1] >= rangeA[0] && rangeB[1] <= rangeA[1])
+  );
+};
